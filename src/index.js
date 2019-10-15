114d69486cfc9cce10ad6d28c418227639d4dab5
xengus <serjiox13@gmail.com>
2019-10-15 22:51:26 +0300

    update index.js

--- src/index.js
+++ src/index.js
@@ -1,3 +1,13 @@
 module.exports = function check(str, bracketsConfig) {
-  // your solution
+  const brackets = bracketsConfig.map(elem => elem.join(''));
+  function  outPut(string)  {
+    let result = string;
+    brackets.forEach((br) => {
+      result = result.replace(br, ''); 
+      });
+    if (result.length === 0) return true;
+    if (string.length === result.length) return false;
+    return outPut(result, brackets);
+  };
+  return outPut(str, brackets); 
 }
